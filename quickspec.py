596e8be8705dbd69122481a7c3f6c578a4bfa481
Pascal LOUIS <plouis35@gmail.com>
2024-10-24 18:52:08 +0200

    removed test code

--- quickspec.py
+++ quickspec.py
@@ -1,12 +1,8 @@
 __version__ = '0.1'
-
-import os, sys
 import logging
-import tkinter as tk
-from tkinter import ttk
 from app.main import Application
 
 if __name__ == "__main__":
     app = Application(__version__)
-    logging.info("QuickSpec started")
+    logging.info("QuickSpec ready")
     app.mainloop()
