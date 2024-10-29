75b0ac1d9346f1ae8e18f40d6ad218d0fc9b40b9
Pascal LOUIS <plouis35@gmail.com>
2024-10-29 19:00:21 +0100

    first specreduce tests

--- spc/spectrum.py
+++ spc/spectrum.py
@@ -8,27 +8,30 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Button
 import matplotlib.pyplot as plt
-from  matplotlib.image import AxesImage
+from matplotlib.image import AxesImage
 from matplotlib.figure import Figure
 from matplotlib.axes import Axes
 
 from astropy.utils.exceptions import AstropyWarning
-from astropy.modeling import models
-
 from astropy import units as u
 from astropy.nddata import CCDData, NDData, StdDevUncertainty
 from astropy.stats import mad_std
 from astropy.io import fits
-from specutils.spectra.spectrum1d import Spectrum1D
+from astropy.modeling import models, fitting
 
-warnings.simplefilter('ignore', category=AstropyWarning)
-warnings.simplefilter('ignore', UserWarning)
+from specutils.spectra.spectrum1d import Spectrum1D
 
 from specreduce.tracing import FlatTrace, FitTrace
 from specreduce.background import Background
 from specreduce.extract import BoxcarExtract
 
+from specreduce import WavelengthCalibration1D
+
+
 from app.config import Config
+
+warnings.simplefilter('ignore', category=AstropyWarning)
+warnings.simplefilter('ignore', UserWarning)
 
 class spec1d:
 
@@ -54,14 +57,18 @@ class spec1d:
                                   peak_method = 'gaussian', 
                                   window = 50) #, guess=605) #, guess=407)
 
+        logging.info('trace fitted')
         #trace_model : one of Chebyshev1D, Legendre1D, Polynomial1D, or Spline1D
         #peak_method : One of gaussian, centroid, or max. gaussian
         _bg_separation = 80
         _bg_width = 50
         _trace_width = 15
         bg: Background = Background.two_sided(master_science, sci_tr, separation=_bg_separation, width=_bg_width) 
+        logging.info('background extracted')
         extract = BoxcarExtract(master_science - bg, sci_tr, width = _trace_width)
+        logging.info('background substracted')
         sci_spectrum: Spectrum1D = extract.spectrum
+        logging.info('spectrum extracted')
 
         self._spec2d_axe.imshow(bg.bkg_wimage, origin='lower', aspect='auto', cmap=plt.cm.gray, alpha=0.2)
         self._spec2d_axe.imshow(sci_tr.image.data, origin='lower', aspect='auto', cmap=plt.cm.gray, alpha=0.2)
@@ -73,6 +80,41 @@ class spec1d:
         self._spec1d_axe.step(sci_spectrum.spectral_axis , sci_spectrum.flux, color='red', linewidth = '0.3')
         self._spec1d_axe.set_xlabel('Pixels')
         self._spec1d_axe.set_ylabel('ADU')
+
+        logging.info('calibrating neon spectrum...')
+
+        self._spec1d_axe.clear()
+
+        wavelength = [6506.53, 6532.88, 6598.95, 6678.28, 6717.04]*u.AA
+        pixels = [770, 1190, 2240, 3484, 4160]*u.pix
+
+        #line_list = QTable([pixels, wavelength], names=["pixel_center", "wavelength"])
+        #input_spectrum, matched_line_list=None, line_pixels=None, line_wavelengths=None, catalog=None, input_model=Linear1D(), fitter=None
+        cal = WavelengthCalibration1D(input_spectrum = sci_spectrum,
+            #matched_line_list = line_list,
+            line_wavelengths = wavelength,
+            line_pixels = pixels,
+            #input_model = models.Polynomial1D(degree = 2),
+            #fitter = fitting.LMLSQFitter()
+            fitter = fitting.LinearLSQFitter()
+            )
+        
+        print('residuals :', str(cal.residuals) )
+        print('fitted ', str(cal.fitted_model) )
+
+        logging.info('spectrum calibrated')
+
+        calibrated_spectrum = cal.apply_to_spectrum(sci_spectrum)
+
+        self._spec1d_axe.set_ylabel('Relative intensity')
+        self._spec1d_axe.set_xlabel('Wavelength (ang)')
+
+        self._spec1d_axe.step(calibrated_spectrum.spectral_axis, calibrated_spectrum.flux, color='green', linewidth = '0.3')
+
+        #logging.info('residuals :', cal.residuals )
+        #logging.info('fitted ', cal.fitted_model )
+
+
 
 
 
